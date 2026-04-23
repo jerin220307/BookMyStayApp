c213357972780478f89f3a9d87e6de5a8eadf040
jerin220307 <jerin220307@gmail.com>
2026-04-23 22:00:48 +0530

    UC8: Booking history and reporting

--- App/src/RoomService.java
+++ App/src/RoomService.java
@@ -1,5 +1,13 @@
-public void addService(String service) {
-    System.out.println("Service added: " + service);
+List<Booking> history = new ArrayList<>();
+
+public void addToHistory(Booking b) {
+    history.add(b);
+}
+
+public void showHistory() {
+    for (Booking b : history) {
+        System.out.println(b.name + " booked room " + b.roomId);
+    }
 }
 
 void main() {
